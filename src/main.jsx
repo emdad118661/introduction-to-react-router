1425e884c9d213466e83d9cbf652324a88b45021
emdad118661 <emdad118661@gmail.com>
2024-04-23 20:56:54 +0600

    posts post post details

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import Contact from './Components/Contact/Contact.jsx';
 import About from './Components/About/About.jsx';
 import Users from './Components/Users/Users.jsx';
 import UserDetails from './Components/UserDetails/UserDetails.jsx';
+import Posts from './Components/Posts/Posts.jsx';
+import PostDetails from './Components/PostDetails/PostDetails.jsx';
 
 // import Contact from "./routes/contact";
 
@@ -37,9 +39,17 @@ const router = createBrowserRouter([
         path: "/users/:userId",
         loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
         element: <UserDetails></UserDetails>
+      },
+      {
+        path: "/posts",
+        loader: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+        element: <Posts></Posts>
+      },
+      {
+        path: "/post/:postId",
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element: <PostDetails></PostDetails>
       }
-
-
     ]
   },
   
